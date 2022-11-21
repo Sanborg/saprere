ef6b006d2270ef18b607726f1e9b5f3a8b626b3d
Sanborg <rafasanc.rj@gmail.com>
2022-11-21 16:22:36 -0300

    Push contendo criação e formatação das telas.

---
+++ sapere/sapere/frmVisualizacaoEventoRespondidoParte1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace sapere.View
+{
+    /// <summary>
+    /// Lógica interna para frmVisualizacaoEventoRespondidoParte1.xaml
+    /// </summary>
+    public partial class frmVisualizacaoEventoRespondidoParte1 : Window
+    {
+        public frmVisualizacaoEventoRespondidoParte1()
+        {
+            InitializeComponent();
+        }
+    }
+}
